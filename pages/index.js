0ac1474b39e68e529743af67ad0daf3ae0851a7c
ericdepaula <ericdepaula39@gmail.com>
2022-11-11 12:09:11 -0300

    Add Banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
-import Menu from "../src/components/Menu"; 
+import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
@@ -48,18 +48,34 @@ const StyledHeader = styled.div`
         border-radius: 50%;
     }
     .user-info {
-        margin-top: 50px;
+        /* margin-top: 50px; */
         display: flex;
         align-items: center;
         width: 100%;
         padding: 16px 32px;
         gap: 16px;
     }
+	.banner{
+      position: flex;
+      width: 1512px 100%;
+      height: 230px;
+      left: 0px;
+      top: 56px;
+
+      background: url("https://images.unsplash.com/photo-1450849608880-6f787542c88a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1121&q=80");
+	}
 `;
+// function Banner() {
+// 	return (
+// 		<StyledHeader>
+// 			<section className="banner"></section>
+// 		</StyledHeader>
+// 	)
+// }
 function Header() {
 	return (
 		<StyledHeader>
-			{/* <img src="banner" /> */}
+			<div className="banner"></div>
 			<section className="user-info">
 				<img src={`https://github.com/${config.github}.png`} />
 				<div>
@@ -76,7 +92,7 @@ function Header() {
 }
 
 function Timeline(propriedades) {
-
+ 
 	const playlistNames = Object.keys(propriedades.playlists);
 
 	return (
